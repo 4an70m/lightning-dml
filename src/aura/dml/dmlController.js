37490e8b1683888296c0c0ce30d8db7ffb4d581b
Alex <4an70m@gmail.com>
2018-09-14 01:07:50 +0300

    Prototype check access

--- src/aura/dml/dmlController.js
+++ src/aura/dml/dmlController.js
@@ -3,22 +3,20 @@
  */
 ({
     doInit: function (cmp, evt, helper) {
-        cmp.get("v.context").dml = {
-            query: (query) => {
-                return new Promise($A.getCallback(function(resolve, reject) {
-                    const [a, b] = arguments;
-                    let action = cmp.get("c.query");
+        window.dml = {
+            query: (query) =>
+                new Promise($A.getCallback(function (resolve, reject) {
+                    const action = cmp.get("c.query");
                     action.setParams({"query": query});
                     action.setCallback(this, result => {
                         resolve(result.getReturnValue());
                     });
                     $A.enqueueAction(action);
-                }));
-            },
+                })),
 
             update: (sobjects) => {
-                return new Promise($A.getCallback(function(resolve, reject) {
-                    let action = cmp.get("c.performUpdate");
+                return new Promise($A.getCallback(function (resolve, reject) {
+                    const action = cmp.get("c.performUpdate");
                     action.setParams({"sobjects": sobjects});
                     action.setCallback(this, result => {
                         resolve(result.getReturnValue());
